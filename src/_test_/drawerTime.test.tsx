e49b29748fc77367d8cbd128f6dc864004b6c14e
rodolphe37 <rodolphe.a@gmail.com>
2022-12-28 15:10:06 +0100

    FEATURE: added some other unit tests

---
+++ src/_test_/drawerTime.test.tsx
@@ -0,0 +1,12 @@
+import HomePage from 'Pages/HomePage'
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '../testUtils'
+
+describe('Drawer time is present in Home page', () => {
+  it('If drawerIsOpen is true, should appear drawer component to screen', async () => {
+    render(<HomePage drawerISOpen={true} />)
+
+    const drawerComponent = screen.getByTestId('drawer')
+    expect(drawerComponent).toBeInTheDocument()
+  })
+})
